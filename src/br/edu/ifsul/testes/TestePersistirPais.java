ec11d78a82987584850672050913d9ebc0593b75
Claudinei <Claudinei@Claudinei-PC>
2018-04-19 21:16:41 -0300

    Commit inicial do projeto

---
+++ src/br/edu/ifsul/testes/TestePersistirPais.java
@@ -0,0 +1,36 @@
+package br.edu.ifsul.testes;
+
+import br.edu.ifsul.modelo.Pais;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Claudinei
+ * 19/04/2018 20:37
+ */
+public class TestePersistirPais {
+    
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("IFSULPU");
+        
+        EntityManager em = emf.createEntityManager();
+        
+        // OBJETO
+        Pais pais = new Pais();
+        pais.setNome("Brasil");
+        pais.setIso("BRA");
+
+        // TRANSAÇÃO
+        em.getTransaction().begin();
+        em.persist(pais);
+        em.getTransaction().commit();
+        
+        em.close();
+        emf.close();
+        
+    }
+    
+}
